a56bae063ca9e34aedf28653cc2477c7073a6638
Ertheo Siswadi <esiswadi@g.ucla.edu>
2020-01-21 11:01:46 -0800

    no longer checks for password, only checks if unique, waiting for DB manager

--- src/app.py
+++ src/app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    checker =  signup_checker("hello@@gmail.com", "LAlala.,", "1234A.")
+    checker =  signup_checker("hello@@gmail.com", "LAlala.,")
     return checker.check()
--- src/signup_checker.py
+++ src/signup_checker.py
@@ -1,55 +1,45 @@
 import re
-from password_strength import PasswordPolicy
-
 import sys
 import os
 sys.path.append(os.getcwd() + '/' + 'src/protocol_buffers')
 import signupconfirm_pb2
 
-#debug
-from pprint import pprint
-
 class signup_checker:
-    def __init__(self, email, username, password):
+    def __init__(self, email, playerid):
         self.email = email
-        self.username = username
-        self.password = password
+        self.playerid = playerid
         self.message = signupconfirm_pb2.SignUpConfirmation()
 
-    def username_checker(self):
+    #checks if playerid is unique
+    #checks if playerid has swear words
+    def playerid_checker(self):
         #does it exist in the database
-        #TODO: call the DB manager to check if username exists
-        username_unique = False
+        #TODO: call the DB manager to check if playerid exists
+        playerid_not_unique = False
+        #TODO: check if email exists (valid)
+        playerid_not_clean = False
 
-        if(len(self.username) <= 2 or len(self.username) >= 20):
-            self.message.username = self.message.UsernameCode.length
-        if(not username_unique):
-            self.message.username = self.message.UsernameCode.unique
+        if(playerid_not_unique):
+            self.message.playerid = self.message.ErrorCode.notunique
+        if(playerid_not_clean):
+            self.message.playerid = self.message.ErrorCode.notclean
 
+    # checks if email is unique
+    # checks if email is valid
     def email_checker(self):
-        email_regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
-        self.message.email = False
-        if(re.search(email_regex, self.email)):
-            self.message.email = True
+        #TODO: call the DB manager to check if email exists
+        email_not_unique = False
+        #TODO: check if email exists (valid)
+        email_exists = False
 
-    def password_checker(self):
-        policy = PasswordPolicy.from_names(
-            length=8,  # min length: 8
-            uppercase=2,  # need min. 2 uppercase letters
-            numbers=2,  # need min. 2 digits
-            special=2,  # need min. 2 special characters
-        )
-        self.message.password = ",".join(map(str, policy.test(self.password)))
+        if(email_exists):
+            self.message.email = self.message.ErrorCode.notvalid
+        if(email_not_unique):
+            self.message.email = self.message.ErrorCode.notunique
 
     def check(self):
-        self.password_checker()
         self.email_checker()
-        self.username_checker()
-
-        #debug
-        message2 = signupconfirm_pb2.SignUpConfirmation()
-        message2.ParseFromString(self.message.SerializeToString())
-        pprint(message2.email)
+        self.playerid_checker()
 
         return self.message.SerializeToString()
 
