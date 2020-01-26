46295f622e7a3a8253c13bbe0d258f2af83a7797
SuperE's Workstation <esiswadiworkstation@SuperEss-iMac.local>
2020-01-26 04:17:59 -0800

    can now send email using "chefmoji@toothfairy.com" hehe

---
+++ src/instance/config.py
@@ -0,0 +1,12 @@
+# email token settings
+SECRET_KEY = 'ABCD'
+SECRET_SALT = '1234'
+
+# email settings
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_DEFAULT_SENDER = 'chefmojimoji@gmail.com'
+MAIL_USERNAME = 'chefmojimoji@gmail.com'
+MAIL_PASSWORD = 'IloveChefmoji.1'
